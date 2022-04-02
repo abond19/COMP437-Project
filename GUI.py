2cc28473c7c58d123d1fb166fad5e045c1ec9497
abond100 <abond19@ku.edu.tr>
2022-04-02 17:03:56 +0300

    Framework for classifying gestures, and first gesture implemented

--- GUI.py
+++ GUI.py
@@ -5,7 +5,8 @@ import mediapipe as mp
 from tensorflow.keras.models import load_model
 import cv2
 import sys
-from hand_gestures import HandGestures
+from hand_gestures import HandGestures, Gestures
+import os
 
 class GUI:
     def __init__(self, window, window_title, video_source=0):
@@ -13,8 +14,10 @@ class GUI:
         self.window.title(window_title)
         self.video_source = video_source
         self.image = None
+        self.window.geometry("1000x1000")
         self.hand_gestures = HandGestures()
-
+        self.recently_took_action = False
+        self.update_count = 0
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_hands = mp.solutions.hands   
@@ -23,9 +26,6 @@ class GUI:
         #self.model = load_model("mp_hand_gesture")
 
         self.vid = VideoCapture(video_source)
-
-        self.canvas = tkinter.Canvas(window, width=self.vid.width, height=self.vid.height)
-        self.canvas.pack()
 
         self.delay = 5
         self.update()
@@ -36,18 +36,27 @@ class GUI:
     
     def update(self):
         ret, image = self.vid.get_frame()
-
+        self.update_count += 1
         if ret:
             image.flags.writeable = False
             #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            photo, results = self.hand_gestures.processImage(image)
+            photo, results, num_hands = self.hand_gestures.processImage(image)
+            gesture = self.hand_gestures.classification(results)
+            self.apply_action(gesture)
+
             if self.image is None:
-                self.image = tkinter.Label(self.window, image=photo)
+                self.image = tkinter.Label(self.window, image=photo, text=f"Gesture: {gesture}", compound=tkinter.BOTTOM)
                 self.image.place(x=0, y=0)
             else:
-                self.image.configure(image=photo)
+                self.image.configure(image=photo, text=f"Gesture: {gesture}")
                 self.image.image = photo
                 #cv2.imshow("Hands", cv2.flip(image, 1))
 
         self.window.after(self.delay, self.update)
 
+    def apply_action(self, gesture):
+        if gesture == Gestures.GESTURE_NONE or gesture is None:
+            self.recently_took_action = False
+        if gesture == Gestures.GESTURE_5 and not self.recently_took_action:
+            os.system("osascript -e 'tell application \"System Events\" to key code 124 using control down'")
+            self.recently_took_action = True
--- hand_gestures.py
+++ hand_gestures.py
@@ -1,10 +1,19 @@
-import tkinter 
 import cv2
 import mediapipe as mp
-import PIL
+import PIL.Image, PIL.ImageTk
+import tkinter
+from enum import Enum
+
+class Gestures(Enum):
+    GESTURE_NONE = 0,
+    GESTURE_1 = 1,
+    GESTURE_2 = 2, 
+    GESTURE_3 = 3,
+    GESTURE_4 = 4,
+    GESTURE_5 = 5
 
 class HandGestures:
-    def __init__(self, model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+    def __init__(self, model_complexity=0, min_detection_confidence=0.7, min_tracking_confidence=0.5):
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_hands = mp.solutions.hands   
@@ -17,6 +26,7 @@ class HandGestures:
         results = self.hands.process(image)
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        num_hands = len(results.multi_hand_landmarks) if results.multi_hand_landmarks else 0
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 self.mp_drawing.draw_landmarks(
@@ -29,4 +39,60 @@ class HandGestures:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(image))
 
-        return photo, results
+        return photo, results, num_hands
+
+    def get_features(self, image):
+        return self.hands.process(image)
+
+    def classification(self, results):
+        if not results.multi_hand_landmarks:
+            return None
+
+        firstFingerOpen, secondFingerOpen, thirdFingerOpen, fourthFingerOpen, fifthFingerOpen = self.get_finger_results(results)
+
+        gesture = self.classify(firstFingerOpen, secondFingerOpen, thirdFingerOpen, fourthFingerOpen, fifthFingerOpen)
+        return gesture
+
+    def classify(self, firstFingerOpen, secondFingerOpen, thirdFingerOpen, fourthFingerOpen, fifthFingerOpen):
+        if firstFingerOpen and secondFingerOpen and thirdFingerOpen and fourthFingerOpen and fifthFingerOpen:
+            return Gestures.GESTURE_5
+        if firstFingerOpen and secondFingerOpen and thirdFingerOpen and fourthFingerOpen:
+            return Gestures.GESTURE_4
+        if firstFingerOpen and secondFingerOpen and thirdFingerOpen:
+            return Gestures.GESTURE_3
+        if firstFingerOpen and secondFingerOpen:
+            return Gestures.GESTURE_2
+        if firstFingerOpen:
+            return Gestures.GESTURE_1
+        return Gestures.GESTURE_NONE
+ 
+    def get_finger_results(self, results):
+        firstFingerOpen = False
+        secondFingerOpen = False
+        thirdFingerOpen = False
+        fourthFingerOpen = False
+        fifthFingerOpen = False
+
+        landmarks = results.multi_hand_landmarks[0]
+        pseudoFixKeyPoint = landmarks.landmark[2].x
+        if landmarks.landmark[3].x < pseudoFixKeyPoint and landmarks.landmark[4].x < pseudoFixKeyPoint:
+            firstFingerOpen = True
+        
+        pseudoFixKeyPoint = landmarks.landmark[6].y
+        if landmarks.landmark[7].y < pseudoFixKeyPoint and landmarks.landmark[8].y < pseudoFixKeyPoint:
+            secondFingerOpen = True
+        
+        pseudoFixKeyPoint = landmarks.landmark[10].y
+        if landmarks.landmark[11].y < pseudoFixKeyPoint and landmarks.landmark[12].y < pseudoFixKeyPoint:
+            thirdFingerOpen = True
+
+        pseudoFixKeyPoint = landmarks.landmark[14].y
+        if landmarks.landmark[15].y < pseudoFixKeyPoint and landmarks.landmark[16].y < pseudoFixKeyPoint:
+            fourthFingerOpen = True
+
+        pseudoFixKeyPoint = landmarks.landmark[18].y
+        if landmarks.landmark[19].y < pseudoFixKeyPoint and landmarks.landmark[20].y < pseudoFixKeyPoint:
+            fifthFingerOpen = True
+
+        return firstFingerOpen, secondFingerOpen, thirdFingerOpen, fourthFingerOpen, fifthFingerOpen
+        
